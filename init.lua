223fdb5e869d59939bb71f8f60024970eb39835e
ugly-one <omojzboze@protonmail.com>
2024-04-03 20:53:48 +0200

    New alias to set-up remote and changes to nvim, god knows what exactly, but stuff seem to work

--- init.lua
+++ init.lua
@@ -1,3 +1,5 @@
+vim.g.mapleader=" "
+vim.g.maplocalleader=" "
 vim.o.background="light"
 vim.wo.number = true
 vim.wo.relativenumber = true
@@ -14,22 +16,21 @@ vim.g.OmniSharp_server_use_net6 = 1 -- I think this one has to be setup before s
 local Plug = vim.fn['plug#']
 vim.call('plug#begin')
 Plug('OmniSharp/omnisharp-vim') -- basic C# support
-Plug('tpope/vim-surround') -- to replace brackets/quotes with something else
+--Plug('tpope/vim-surround') -- to replace brackets/quotes with something else
 Plug('prabirshrestha/asyncomplete.vim') -- not sure why I have this one. Why omnisharp-vim is not enough?
 Plug('junegunn/fzf', { ['dir'] = '~/.fzf', ['do'] = './install --all' }) -- required by fzf-lua
 Plug('ibhagwan/fzf-lua') -- finding files (and probably more!)
 Plug('dense-analysis/ale') -- for showing errors
-Plug('tpope/vim-fugitive') -- testing if doing git stuff from nvim brings any benefits 
+--Plug('tpope/vim-fugitive') -- testing if doing git stuff from nvim brings any benefits 
 vim.call('plug#end')
 
-vim.keymap.set('n', '<C-o><C-d>', '<cmd> OmniSharpGotoDefinition<CR>')
-vim.keymap.set('n', '<C-o><C-i>', '<cmd> OmniSharpFindImplementations<CR>')
-vim.keymap.set('n', '<C-o><C-u>', '<cmd> OmniSharpFindUsages<CR>')
-vim.keymap.set('n', '<C-o><C-r>', '<cmd> OmniSharpRename<CR>')
-vim.keymap.set('n', '<C-Space>', '<cmd> OmniSharpSignatureHelp<CR>')
-vim.keymap.set('i', '<C-Space>', '<cmd> OmniSharpSignatureHelp<CR>')
-vim.keymap.set('n', '<C-o><C-t>', '<cmd> OmniSharpTypeLookup<CR>') 
-vim.keymap.set('n', '<C-o><C-a>', '<cmd> OmniSharpGetCodeActions<CR>')
+vim.keymap.set('n', '<Leader>d', '<cmd> OmniSharpGotoDefinition<CR>')
+vim.keymap.set('n', '<Leader>i', '<cmd> OmniSharpFindImplementations<CR>')
+vim.keymap.set('n', '<Leader>u', '<cmd> OmniSharpFindUsages<CR>')
+vim.keymap.set('n', '<Leader>r', '<cmd> OmniSharpRename<CR>')
+vim.keymap.set('n', '<Leader><Space>', '<cmd> OmniSharpSignatureHelp<CR>')
+vim.keymap.set('n', '<Leader>tl', '<cmd> OmniSharpTypeLookup<CR>') 
+vim.keymap.set('n', '<Leader>ca', '<cmd> OmniSharpGetCodeActions<CR>')
 
 vim.g.asyncomplete_auto_completeopt = 0
 vim.o.completeopt="menuone,noinsert,noselect"
@@ -38,10 +39,11 @@ vim.g.asyncomplete_force_refresh_on_context_changed = 1
 
 vim.g.OmniSharp_highlight_types = 3 
 
-vim.keymap.set('i', '<CR>', function()
-    return vim.fn.pumvisible() == 1 and vim.fn['asyncomplete#close_popup']() or '<CR>'
-end, {expr = true})
+--vim.keymap.set('i', '<CR>', function()
+--    return vim.fn.pumvisible() == 1 and vim.fn['asyncomplete#close_popup']() or '<CR>'
+--end, {expr = true})
 
-vim.keymap.set('n', '<C-t>', '<cmd>:FzfLua files<CR>')
-vim.keymap.set('n', '<C-g>', '<cmd>:FzfLua git_status<CR>')
-vim.wo.statusline='%t %{fugitive#statusline()} Water warrior'
+vim.keymap.set('n', '<Leader>t', '<cmd>:FzfLua files<CR>')
+vim.keymap.set('n', '<Leader>g', '<cmd>:FzfLua git_status<CR>')
+--vim.wo.statusline='%t %{fugitive#statusline()} Water warrior'
+vim.keymap.set('n', '<Leader>js', '<cmd>:%!jq .<CR>')
