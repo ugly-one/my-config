511b02afe4427a4993b6eb4325be27a254bb735c
ugly-one <omojzboze@protonmail.com>
2024-07-26 20:29:29 +0200

    updates I forgot to push

--- init.lua
+++ init.lua
@@ -9,21 +9,25 @@ vim.o.expandtab = true -- use spaces instead of tab
 vim.o.tabstop = 4 -- adding only this setting doesn't make the tab to stop after 4 spaces. Had to add the 2 below.
 vim.o.shiftwidth = 4 -- what does it do? added it because tabstop doesn't work
 vim.o.softtabstop = 4 -- what does it do? added it because tabstop doesn't work
-
+vim.opt.clipboard = 'unnamedplus' -- copy to system's clipboard
+vim.g.nvim_tree_respect_buf_cwd=1 -- open tree window at the location of current buffer
+vim.g.netrw_keepdir=0 -- something that doesn't allow me to copy files ?!
 vim.g.OmniSharp_server_use_net6 = 1 -- I think this one has to be setup before something. Perhaps before we install the server. 
 -- so maybe it's OK to have it after all plugins are installed
 
 local Plug = vim.fn['plug#']
 vim.call('plug#begin')
 Plug('OmniSharp/omnisharp-vim') -- basic C# support
---Plug('tpope/vim-surround') -- to replace brackets/quotes with something else
 Plug('prabirshrestha/asyncomplete.vim') -- not sure why I have this one. Why omnisharp-vim is not enough?
-Plug('junegunn/fzf', { ['dir'] = '~/.fzf', ['do'] = './install --all' }) -- required by fzf-lua
-Plug('ibhagwan/fzf-lua') -- finding files (and probably more!)
+-- having issues with fzf - not able to find files - I get an infinite spinner
+--Plug('junegunn/fzf', { ['dir'] = '~/.fzf', ['do'] = './install --all' }) -- required by fzf-lua
+--Plug('ibhagwan/fzf-lua') -- finding files (and probably more!)
+-- using telescope for now
+Plug('nvim-lua/plenary.nvim')
+Plug('nvim-telescope/telescope.nvim', { ['tag'] = '0.1.8' })
 Plug('dense-analysis/ale') -- for showing errors
 Plug('tpope/vim-fugitive') -- testing if doing git stuff from nvim brings any benefits. So far it's useful to do Git blame
 Plug('catppuccin/nvim',{ ['as'] = 'catppucin' }) -- theme
---Plug('dstein64/vim-startuptime') -- testing why start-up is slow. should be removed once testing is done.
 vim.call('plug#end')
 
 vim.cmd.colorscheme "catppuccin"
@@ -33,14 +37,21 @@ vim.keymap.set('n', '<Leader>i',        '<cmd> OmniSharpFindImplementations<CR>'
 vim.keymap.set('n', '<Leader>u',        '<cmd> OmniSharpFindUsages<CR>')
 vim.keymap.set('n', '<Leader>r',        '<cmd> OmniSharpRename<CR>')
 vim.keymap.set('n', '<Leader><Space>',  '<cmd> OmniSharpSignatureHelp<CR>')
+vim.keymap.set('i', '<C-b>',            '<C-o> <cmd>OmniSharpSignatureHelp<CR>')
 vim.keymap.set('n', '<Leader>tl',       '<cmd> OmniSharpTypeLookup<CR>') 
 vim.keymap.set('n', '<Leader>ca',       '<cmd> OmniSharpGetCodeActions<CR>')
 vim.keymap.set('n', '<Leader>se',       '<cmd> OmniSharpGlobalCodeCheck<CR>')
 vim.keymap.set('n', '<Leader>cf',       '<cmd> OmniSharpCodeFormat<CR>')
 vim.keymap.set('n', '<Leader>rt',       '<cmd> !dotnet test<CR>')
 vim.keymap.set('n', '<Leader>b',        '<cmd> :wa <BAR> !dotnet build<CR>') -- time to learn how to write lua functions?
-vim.keymap.set('n', '<Leader>rct',      '<cmd> OmniSharpRunTest<CR>')
-vim.keymap.set('n', '<Leader>do',        '<cmd> OmniSharpDocumentation<CR>')
+vim.keymap.set('n', '<Leader>rct',      '<cmd> :wa <BAR> OmniSharpRunTest<CR>')
+vim.keymap.set('n', '<Leader>do',       '<cmd> OmniSharpDocumentation<CR>')
+
+vim.keymap.set('n', '<Leader>op', function() 
+    print("test", false)
+    print("hahahaha")
+    --vim.fn.setqflist([], 'r', {'title': 'My search'})
+end)
 
 vim.g.asyncomplete_auto_completeopt = 0
 vim.o.completeopt="menuone,noinsert,noselect"
@@ -56,9 +67,10 @@ vim.g.OmniSharp_loglevel = 'error'
 --    return vim.fn.pumvisible() == 1 and vim.fn['asyncomplete#close_popup']() or '<CR>'
 --end, {expr = true})
 
-vim.keymap.set('n', '<Leader>t', '<cmd>:FzfLua files<CR>')
-vim.keymap.set('n', '<Leader>g', '<cmd>:FzfLua git_status<CR>')
-vim.keymap.set('n', '<Leader>f', '<cmd>:FzfLua grep_project<CR>')
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>t', builtin.find_files, {})
+vim.keymap.set('n', '<leader>g', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<Leader>js', '<cmd>:%!jq .<CR>')
 
 -- opening and closing quickfix list
